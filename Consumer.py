0fa6a21f4fb83a7c22ef0610a99fe44ee3902d72
Голиков Егор Николаевич <e.golikov@fabrikant.office>
2021-11-29 17:55:00 +0300

    Add logic for test shards

--- Consumer.py
+++ Consumer.py
@@ -85,134 +85,20 @@ def add_update_data(data_from_db):
 
 def on_message(channel, method_frame, header_frame, body):
     data_from_queue = ast.literal_eval(body.decode('utf-8'))
-    all_data_from_bi_pe_check = get_data_from_db_firm(mc.MS_BI_PE, q.get_data_from_bi_pe_check, as_dict=True,
-                                                      event_gid_str=add_quotes(data_from_queue["event_gid"]))
+    if 0 <= data_from_queue["event_gid"] % 256 <= 128:
+        all_data_from_bi_pe_check = get_data_from_db_firm(mc.MS_BI_PE_1, q.get_data_from_bi_pe_check_1, as_dict=True,
+                                                          event_gid_str = add_quotes(data_from_queue["event_gid"]))
+    else:
+        all_data_from_bi_pe_check = get_data_from_db_firm(mc.MS_BI_PE_2, q.get_data_from_bi_pe_check_2, as_dict=True,
+                                                          event_gid_str=add_quotes(data_from_queue["event_gid"]))
     if all_data_from_bi_pe_check:
         for data_from_db in all_data_from_bi_pe_check:
             add_update_data(data_from_db)
 
-    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+    #channel.basic_ack(delivery_tag=method_frame.delivery_tag)
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.basic_consume('export_event_complete_bi', on_message, auto_ack=False)
 
 channel.start_consuming()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-# Информация по локальному подключению монги
-client = pymongo.MongoClient('mongodb://10.31.86.41:27017')
-db = client['qlik-SilMash']
-
-# Информация по прослушиваемой очереди
-credentials = pika.PlainCredentials('services', 'Cdte8cVr7RhHQWdq')
-params = pika.ConnectionParameters('10.31.82.122', 5672, 'procedure_version_2', credentials)
-
-# Загрузка документа в монгу
-def insert_document(collection, data):
-    return collection.insert(data, check_keys=False)
-
-# Готовим данные для запроса
-def add_quotes(a):
-    return f"'{str(a)}'"
-
-# Достаем данные из БД
-def get_data_from_db_firm(cnx, query, as_dict=False, **kwargs):
-    connect = mc(connection=cnx)
-    with connect.open() as c:
-        result = c.execute_query(query.format(**kwargs), dicted=as_dict)
-    return result
-
-# Способ дешифровки данных для БД Fabrikant текстовых данных
-def decode_data_txt(data):
-    base64_bytes = data.encode("ascii")
-    decode = base64.b64decode(base64_bytes)
-    decode = zlib.decompress(decode)
-    decode = unserialize(decode, object_hook=phpobject)
-    decode = loads(dumps(decode), object_hook=phpobject, decode_strings=True)
-    decode = convert_member_dict(decode)
-    decode = json.dumps(decode, iterable_as_array=True)
-    decode = json.loads(decode)
-    return decode
-
-def on_message(channel, method_frame, header_frame, body):
-    data_from_queue = ast.literal_eval(body.decode('utf-8'))
-    all_data_from_bi_pe_check = get_data_from_db_firm(mc.MS_BI_PE, q.get_data_from_bi_pe_check, as_dict=True,
-                                                      event_gid_str=add_quotes(data_from_queue["event_gid"]))
-    if all_data_from_bi_pe_check:
-        for i in all_data_from_bi_pe_check:
-            i['data'] = decode_data_txt(i['data'])
-            fabrikant_data_collection = db[i['entity']]
-            finding_info = fabrikant_data_collection.find_one({'event_gid': i['event_gid'], 'entity': i['entity']})
-            if finding_info:
-                result_update = fabrikant_data_collection.update_one({"_id": finding_info['_id']},
-                                                                     {"$set": {'data': i['data']}})
-                if result_update.matched_count > 0:
-                    print("Success update event_gid " + str(i['event_gid']) + ' ' + str(i['entity']))
-                    pass
-                else:
-                    print("Failure update")
-                    pass
-            else:
-                insert_document(fabrikant_data_collection, i)
-                print("Success insert event_gid " + str(i['event_gid']) + ' ' + str(i['entity']))
-    #channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-
-credentials = pika.PlainCredentials('services', 'Cdte8cVr7RhHQWdq')
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.31.82.122', 5672, 'procedure_version_2', credentials))
-channel = connection.channel()
-channel.basic_consume('export_event_complete_bi', on_message, auto_ack=False)
-try:
-    channel.start_consuming()
-except KeyboardInterrupt:
-    channel.stop_consuming()
-connection.close()
-
-
-
-'''
--- queries.py
+++ queries.py
@@ -1,15 +1,8 @@
-get_data_from_bi_pe = '''SELECT *
-FROM procedure_export_1.bi bi 
-UNION
-SELECT *
-FROM procedure_export_2.bi bi 
-'''
-
-get_data_from_bi_pe_check = '''SELECT *
-FROM procedure_export_1.bi bi 
+get_data_from_bi_pe_check_1 = '''SELECT *
+FROM procedure_export_1.bi
 WHERE bi.event_gid = ({event_gid_str})
-UNION
-SELECT *
-FROM procedure_export_2.bi bi 
+'''
+get_data_from_bi_pe_check_2 = '''SELECT *
+FROM procedure_export_2.bi
 WHERE bi.event_gid = ({event_gid_str})
 '''
